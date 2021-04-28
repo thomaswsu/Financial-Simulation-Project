1dbd8161b8a93450c3fbce769a46354a14809508
SavioroftheMeeple <mcdonm8@rpi.edu>
2021-04-28 16:45:02 -0400

    rewrite for working deltas
    changed the precious CRM to a newer version. it is probably slower, but it looks like it works

--- Checkpoint 3/checkpoint3.R
+++ Checkpoint 3/checkpoint3.R
@@ -23,7 +23,7 @@ delta.hedge.CRN <- function(M,N,S0,K,r,sigma,t,mu,alpha,b,volvol,V0,call, barrie
   
   # Simulate the paths and deltas...also want to save Z's:
   #heston
-  X <- V <- deltas <- Zmat <- matrix(NA,ncol=N+1,nrow=M)
+  X <- V <- Zmat <- matrix(NA,ncol=N+1,nrow=M)
   X[,1] <- S0
   Zmat[,1] <- 0
   dt <- t/N
@@ -48,20 +48,10 @@ delta.hedge.CRN <- function(M,N,S0,K,r,sigma,t,mu,alpha,b,volvol,V0,call, barrie
   getLastElement <- function(vec){
     return(vec[length(vec)])
   }
-  
-  
   Sbar.Euro <- apply(X, 1, getLastElement) - K # Get last value
   tmp <- which(Sbar.Euro > 0)
   Sbar.Euro[-tmp] <- 0
   P.Euro <- mean(Sbar.Euro)
-  
-  
-  
-  
-  # Sbar.A <- apply(X,1,mean) - K
-  # tmp <- which(Sbar.A > 0)
-  # Sbar.A[-tmp] <- 0
-  # P.Asian <- mean(Sbar.A)
   
   
   # ======================
@@ -73,24 +63,21 @@ delta.hedge.CRN <- function(M,N,S0,K,r,sigma,t,mu,alpha,b,volvol,V0,call, barrie
     f <- function(S,K){
       f <- pmax(S - K,0)
     }
-  }
-  else if(call == 2){  # Up and out barrier Euro call option { 
+  }else if(call == 2){  # Up and out barrier Euro call option { 
     Smax <- apply(S,1,max)
     f <- function(ST, Smax, K) {
       f <- pmax(ST - K,0) # First, treat like regular call
       f[Smax >= barrier] = 0 # Remove if Smax exceeds b
       return(f)
     }
-  }
-  else if(call == 3) {
+  }else if(call == 3) {
     Smin <- apply(S, 1, min)
     f <- function(ST, Smin, K) {
       f <- pmax(K - ST, 0) # First, treat like regular put
       f[Smin <= barrier] = 0 # Remove is Smin is less than b
       return(f)
     }
-  }
-  else { # Euro Pu
+  }else { # Euro Pu
     f <- function(S,K){
       f <- pmax(K - S,0)
     }
@@ -101,42 +88,28 @@ delta.hedge.CRN <- function(M,N,S0,K,r,sigma,t,mu,alpha,b,volvol,V0,call, barrie
   
   # 5. Finally compute the deltas over time:
   deltas.A <- matrix(NA,ncol=N+1,nrow=M)
-  for (i in 1:N){
-    
-    # 6. Save a matrix of the cumulative sums of Z from t_j to T
-    if (i < N){
-      message(print(i))
-      Zmat.sum <- matrix(NA,nrow=M,ncol=N+1)
-      Zmat.sum[,c((i+1):(N+1))] <- t(apply(Zmat[,c((i+1):(N+1))],1,cumsum))
-    }
-    else{
-      Zmat.sum <- matrix(NA,nrow=M,ncol=N+1)
-      Zmat.sum[,c((i+1):(N+1))] <- Zmat[,N+1]
-    }
-    
-    # 7. Save the innovatinos (last two terms of Eq. (2))
-    RHS <- matrix(NA,nrow=M,ncol=N+1)
-    for (k in i:N){
-      RHS[,k+1] <- (k-i+1)*(r - 0.5*sigma*sigma)*dt + sigma*sqrt(dt)*Zmat.sum[,k+1]
-    }
-    
-    # 8. For each row, use the RHS matrix to compute the forward prices
-    # and combine that with sumS[] to get Sbar.
+  
+  delta_CRN <- function(S,K,r,t,sigma){
+    fd<-rep(0,M)
     for (j in 1:M){
-      S2 <- S2h <- matrix(NA,nrow=M,ncol=N+1)
-      S2 <- exp(RHS + log(S[j,i]))
-      S2[,i] <- S[j,i]
-      Sbar <- (apply(S2[,c(i:(N+1))],1,sum) + sumS[j,i] - S[j,i])/(N+1)
-      p1 <- mean(disc[i]*f(Sbar,K))
-      
-      S2h <- exp(RHS + log(S[j,i]+h))
-      S2h[,i] <- S[j,i] + h
-      Sbarh <- (apply(S2h[,c(i:(N+1))],1,sum) + sumS[j,i] - S[j,i])/(N+1)
-      p2 <- mean(disc[i]*f(Sbarh,K))
-      
-      deltas.A[j,i] <- (p2 - p1)/h
+      ss <- sample(100000000,1)
+      set.seed(ss)
+      ST <- (S[j]-h)*exp((r - 0.5*sigma^2)*t+sigma*sqrt(t)*rnorm(M))
+      set.seed(ss)
+      STh <- (S[j]+h)*exp((r - 0.5*sigma^2)*t+sigma*sqrt(t)*rnorm(M))
+      f0 <- f(ST,K)
+      f0h <- f(STh,K)
+      fd[j] <- exp(-r*t)*mean((f0h - f0) / (2*h))
     }
+    return(fd)
   }
+  for (i in 1:N){
+    ttm <- t - dt*(i-1)
+    deltas.A[,i] <- delta_CRN(X[,i],K,r,ttm,sigma)
+  }
+  
+  # Fill in terminal deltas (1/0):
+  deltas.A[,N+1] <- delta_CRN(X[,N+1],K,r,0,sigma)
   
   
   
@@ -206,5 +179,3 @@ CRN.means <- apply(CRN.1$deltas.A,2,mean)
 CRN.sd <- sqrt(apply(CRN.1$deltas.A,2,var))
 print(CRN.means)
 print(CRN.sd)
-
-# I made changes
